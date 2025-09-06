a262daecc626527bccc8558005a1b023d3d5f4eb
Austin Winger-Kailer <145706646+AustinFWK@users.noreply.github.com>
2025-09-06 11:56:19 -0500

    add about me and import tailwind css to index

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -5,6 +5,11 @@ function App() {
     <>
       <div>
         <h1>About me</h1>
+        <p>
+          Hi! I'm Austin Winger-Kailer, nice to meet you! I'm a full stack web
+          developer with a passion for innovation and solving problems through
+          the means of software.
+        </p>
       </div>
       <div>
         <h1>Experience</h1>
