324b6350d660cd7c72d35fae9816a8f51ff9c5f8
Austin Winger-Kailer <145706646+AustinFWK@users.noreply.github.com>
2025-09-20 23:43:09 -0400

    add flex to hero section

--- portfolio/src/Components/Hero/myHero.tsx
+++ portfolio/src/Components/Hero/myHero.tsx
@@ -4,24 +4,33 @@ import EmailIcon from "@mui/icons-material/Email";
 
 export default function MyHero() {
   return (
-    <div id="hero">
+    <div
+      id="hero"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        textAlign: "center",
+      }}
+    >
       <h1>Welcome to My Portfolio</h1>
-      <div>
-        <img src="placeholder.jpg" alt="Hero Image" />
-      </div>
+      <img src="placeholder.jpg" alt="Hero Image" />
       <p>
         Hi! I'm Austin Winger-Kailer, a full stack web developer with a passion
         for innovation and solving problems through the means of software.
       </p>
-
       <a
         href="https://www.linkedin.com/in/austin-winger-kailer-761170295/"
         target="_blank"
+        rel="noopener noreferrer"
       >
         <LinkedInIcon />
       </a>
-
-      <a href="https://github.com/AustinFWK" target="_blank">
+      <a
+        href="https://github.com/AustinFWK"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <GitHubIcon />
       </a>
       <a href="mailto:austin.wingerkailer0701@gmail.com">
