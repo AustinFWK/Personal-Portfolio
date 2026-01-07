58ed4847e311e4431b362d9cde5e9c99ea8f6231
Austin Winger-Kailer <145706646+AustinFWK@users.noreply.github.com>
2026-01-07 16:58:51 -0500

    style resume in header

--- portfolio/src/Components/Nav/navBar.tsx
+++ portfolio/src/Components/Nav/navBar.tsx
@@ -4,7 +4,14 @@ export default function NavBar() {
   return (
     <nav>
       <div>
-        <div style={{ display: "flex", justifyContent: "flex-end" }}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            paddingTop: "1rem",
+            paddingRight: "2rem",
+          }}
+        >
           <Link href="SWE_resume.pdf" target="_blank" rel="noopener noreferrer">
             Resume
           </Link>
