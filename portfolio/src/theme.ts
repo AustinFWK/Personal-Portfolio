80d81b47bb8a3977774b5aa1d08ac8ac109edbed
Austin Winger-Kailer <145706646+AustinFWK@users.noreply.github.com>
2026-01-07 17:00:38 -0500

    add font styling to theme

--- portfolio/src/theme.ts
+++ portfolio/src/theme.ts
@@ -35,4 +35,27 @@ export const Theme = createTheme({
       },
     },
   },
+
+  typography: {
+    fontFamily: "Inter, Roboto, Helvetica, Arial, sans-serif",
+
+    h1: {
+      fontWeight: 700,
+    },
+    h2: {
+      fontWeight: 600,
+    },
+    h3: {
+      fontWeight: 600,
+    },
+    h4: {
+      fontWeight: 500,
+    },
+    body1: {
+      fontWeight: 400,
+    },
+    body2: {
+      fontWeight: 400,
+    },
+  },
 });
