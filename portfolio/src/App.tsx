c997da7bbf28f8cc1cee9b4a302fff09546ce93d
Austin Winger-Kailer <145706646+AustinFWK@users.noreply.github.com>
2026-01-07 17:34:03 -0500

    add fade animation to portfolio

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -3,21 +3,31 @@ import MyExperience from "./Components/Experience/myExperience.tsx";
 import MyProjects from "./Components/Projects/myProjects.tsx";
 import MyHero from "./Components/Hero/myHero.tsx";
 import NavBar from "./Components/Nav/navBar.tsx";
+import { Fade } from "@mui/material";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
+
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        gap: "4rem",
-      }}
-    >
-      <NavBar />
-      <MyHero />
-      <MyExperience />
-      <MyProjects />
-    </div>
+    <Fade in={loaded} timeout={1100}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "4rem",
+        }}
+      >
+        <NavBar />
+        <MyHero />
+        <MyExperience />
+        <MyProjects />
+      </div>
+    </Fade>
   );
 }
 
