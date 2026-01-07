9c9c6bd8691bc9800d22c41e40c8cca733ed7074
Austin Winger-Kailer <145706646+AustinFWK@users.noreply.github.com>
2026-01-06 21:09:42 -0500

    update styling to card components

--- portfolio/src/Components/Experience/myExperience.tsx
+++ portfolio/src/Components/Experience/myExperience.tsx
@@ -6,20 +6,10 @@ export default function MyExperience() {
   return (
     <section id="experience">
       <h1 style={{ textAlign: "center", marginBottom: "1rem" }}>Experience</h1>
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
-          gap: "2rem",
-          padding: "2rem",
-          justifyItems: "center",
-        }}
-      >
-        {/*<div className="card-layout">*/}
+      <div className="card-layout">
         <DevYoungstown />
         <SWEYSU />
         <UndergraduateResearch />
-        {/*</div>*/}
       </div>
     </section>
   );
