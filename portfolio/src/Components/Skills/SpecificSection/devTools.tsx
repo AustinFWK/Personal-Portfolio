176b883b3d329aec2293ec092953f8d7bc8f9f19
Austin Winger-Kailer <145706646+AustinFWK@users.noreply.github.com>
2026-01-13 13:45:10 -0500

    create data directory and experience data types

--- portfolio/src/Components/Skills/SpecificSection/devTools.tsx
+++ portfolio/src/Components/Skills/SpecificSection/devTools.tsx
@@ -1,4 +1,7 @@
 import { Typography, Card, CardContent, Box } from "@mui/material";
+import { FaGithub } from "react-icons/fa";
+import { VscVscodeInsiders } from "react-icons/vsc";
+import { SiXcode } from "react-icons/si";
 
 export default function DevTools() {
   return (
@@ -8,6 +11,18 @@ export default function DevTools() {
           <Typography variant="h4" component="div" align="center">
             Dev Tools
           </Typography>
+          <Box
+            display="flex"
+            flexDirection="row"
+            justifyContent="center"
+            alignItems="center"
+            flexWrap="wrap"
+            marginTop="1rem"
+          >
+            <FaGithub size={40} style={{ margin: "0.5rem" }} />
+            <VscVscodeInsiders size={40} style={{ margin: "0.5rem" }} />
+            <SiXcode size={40} style={{ margin: "0.5rem" }} />
+          </Box>
         </Box>
       </CardContent>
     </Card>
