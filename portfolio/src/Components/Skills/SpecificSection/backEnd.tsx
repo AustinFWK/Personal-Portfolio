7847e9b0da06dd4ccd36ad9fb3785e77f06a0a0c
Austin Winger-Kailer <145706646+AustinFWK@users.noreply.github.com>
2026-01-13 11:54:58 -0500

    add backend skills

--- portfolio/src/Components/Skills/SpecificSection/backEnd.tsx
+++ portfolio/src/Components/Skills/SpecificSection/backEnd.tsx
@@ -1,4 +1,6 @@
 import { Typography, Card, CardContent, Box } from "@mui/material";
+import { FaPython } from "react-icons/fa";
+import { SiSqlite, SiFastapi, SiExpress } from "react-icons/si";
 
 export default function BackEnd() {
   return (
@@ -8,6 +10,19 @@ export default function BackEnd() {
           <Typography variant="h4" component="div" align="center">
             Back-End
           </Typography>
+          <Box
+            display="flex"
+            flexDirection="row"
+            justifyContent="center"
+            alignItems="center"
+            flexWrap="wrap"
+            marginTop="1rem"
+          >
+            <FaPython size={40} style={{ margin: "0.5rem" }} />
+            <SiFastapi size={40} style={{ margin: "0.5rem" }} />
+            <SiExpress size={40} style={{ margin: "0.5rem" }} />
+            <SiSqlite size={40} style={{ margin: "0.5rem" }} />
+          </Box>
         </Box>
       </CardContent>
     </Card>
