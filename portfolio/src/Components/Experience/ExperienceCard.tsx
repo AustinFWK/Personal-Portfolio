92ccd1dd06f11a9211b881eb0dc4d20427ca1170
Austin Winger-Kailer <145706646+AustinFWK@users.noreply.github.com>
2026-01-13 22:01:50 -0500

    create reusable card component

--- portfolio/src/Components/Experience/ExperienceCard.tsx
+++ portfolio/src/Components/Experience/ExperienceCard.tsx
@@ -2,22 +2,76 @@ import DevYoungstown from "./devYoungstown";
 import SWEYSU from "./sweYSU";
 import UndergraduateResearch from "./undergradResearch";
 import { Typography, Box, Card, CardContent } from "@mui/material";
+import type { experience } from "../../Data/experience";
 
-export default function ExperienceCard() {
+interface ExperienceCardProps {
+  experience: experience;
+}
+
+export default function ExperienceCard({ experience }: ExperienceCardProps) {
+  const {
+    organization,
+    title,
+    dateRange,
+    description,
+    svgIcon,
+    secondarySvgIcon,
+  } = experience;
+
   return (
-    <section id="experience">
-      <Typography
-        component="div"
-        variant="h3"
-        style={{ textAlign: "center", marginBottom: "4rem" }}
+    <Card sx={{ maxWidth: 345, marginBottom: "2rem" }}>
+      <Box
+        sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
       >
-        Experience
-      </Typography>
-      <div className="card-layout">
-        <DevYoungstown />
-        <SWEYSU />
-        <UndergraduateResearch />
-      </div>
-    </section>
+        <img
+          src={svgIcon}
+          alt={`${organization} logo`}
+          style={{
+            height: "25%",
+            width: "25%",
+            marginLeft: "auto",
+            marginRight: "auto",
+            marginTop: "1rem",
+          }}
+        />
+        {secondarySvgIcon && (
+          <img
+            src={secondarySvgIcon}
+            alt={`${organization} secondary logo`}
+            style={{
+              height: "25%",
+              width: "25%",
+              marginLeft: "auto",
+              marginRight: "auto",
+              marginTop: "1rem",
+            }}
+          />
+        )}
+
+        <CardContent>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              textAlign: "center",
+            }}
+          >
+            <Typography gutterBottom variant="h4">
+              {organization}
+            </Typography>
+            <Typography gutterBottom variant="h5" component="div">
+              {title}
+            </Typography>
+            <Typography gutterBottom variant="h6" component="div">
+              {dateRange}
+            </Typography>
+            <Typography variant="body2" sx={{ color: "text.secondary" }}>
+              {description}
+            </Typography>
+          </Box>
+        </CardContent>
+      </Box>
+    </Card>
   );
 }
